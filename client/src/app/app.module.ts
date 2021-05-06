b73bc82aadea804f851e3eabbe1d6d0029cb150c
pdcstrong <37966095+pdcstrong@users.noreply.github.com>
2021-05-06 23:41:46 +0300

    End of section 9

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -18,6 +18,10 @@ import { TestsErrorsComponent } from './errors/tests-errors/tests-errors.compone
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [
@@ -31,7 +35,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     MessagesComponent,
     TestsErrorsComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberCardComponent
    // AppRoutingModule
   ],
   imports: [
@@ -43,10 +48,14 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
-    })
+    }),
+    TabsModule.forRoot(),
+    NgxGalleryModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS , useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS , useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS , useClass: JwtInterceptor, multi: true}
+ 
   ],
   bootstrap: [AppComponent]
 })
